7fd07bfd5ba3508882e55b803e37db18570117d9
SHIVARAM2208 <167754355+SHIVARAM2208@users.noreply.github.com>
2025-06-21 23:05:09 +0530

    Update jwt.js

--- api/jwt.js
+++ api/jwt.js
@@ -1,15 +1,17 @@
-import jwt from 'jsonwebtoken';
+const jwt = require("jsonwebtoken");
 
 const SECRET = process.env.JWT_SECRET || 'default_secret_change_this';
 
-export function signJwt(payload) {
+function signJwt(payload) {
   return jwt.sign(payload, SECRET, { expiresIn: "2h" });
 }
 
-export function verifyJwt(token) {
+function verifyJwt(token) {
   try {
     return jwt.verify(token, SECRET);
   } catch {
     return null;
   }
 }
+
+module.exports = { signJwt, verifyJwt };
